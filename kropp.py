120fb7320dfedeefbf7d73128d673aa5540f4366
Johan Andersson <johan.andersson2@yandex.com>
2018-07-29 15:43:59 +0200

    Lade till spelare och huvudloop

--- kropp.py
+++ kropp.py
@@ -13,3 +13,16 @@ rad1 = ["o", " ", " "]
 rad2 = [" ", " ", "x"]
 plan = [rad0, rad1, rad2]
 spelplan()
+tur = "x"
+
+while(True):
+    print("Det är spelare " + tur + " tur.")
+    r = int(input("Välj rad: "))
+    k = int(input("Välj kolumn: "))
+    plan[r-1][k-1] = tur
+    spelplan()
+
+    if(tur == "x"):
+        tur = "o"
+    else:
+        tur = "x"
